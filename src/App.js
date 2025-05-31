28470cbf8211b73891e6a88398c75f28d7c7145a
chandler Grajeda <chandlerg25@gmail.com>
2025-05-30 20:20:36 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'Learn React', completed: 'Completed' },
+    { id: 2, text: 'Build a Todo App', completed: 'Completed' },
+    { id: 3, text: 'Submit Assignment', completed: 'Completed' }
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>To Do Application</h1>
   );
 }
 
