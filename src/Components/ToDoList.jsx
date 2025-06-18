a32f32808fd82f4c7cb8b59823339846493b0646
chandler Grajeda <chandlerg25@gmail.com>
2025-06-18 12:14:55 -0500

    ToDoList update

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
 
 export default function ToDoList({todos,SetTodos}) {
-    console.log (todos)
-  return (
-    <div>ToDoList</div>
-  )
+    function handleDelete (event, todosID) {
+    console.log (event)
+    console.log (todosID)
+    const result = todos.filter(todos=>todos.id!==todosID)
+    setTodos (result)
+  }
 }
+
+  function handleUpdate (event, TodosID) {
+    const updatedTodos = todos.map(todos => {                        
+      console.log ("The id of the Todos: ", todos.id)
+      console.log ("The ID of row that need to be changed", TodosID)
+      if (todos.id === TodosID) {                                     
+        return {                                                   
+          ...todos,
+          status: 'read'
+        };
+      }
+      return todos;                                                 
+    });
+    setTodos(updatedTodos);
+  }
