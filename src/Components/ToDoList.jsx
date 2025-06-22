c3232b70b2f7fed015f8d27b6e2d497cd3c416b5
chandler Grajeda <chandlerg25@gmail.com>
2025-06-22 13:45:48 -0500

    finishing todo list with buttons working

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -2,26 +2,59 @@ import React from 'react'
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 
-export default function ToDoList({todos,SetTodos}) {
-    function handleDelete (event, todosID) {
+
+export default function ToDoList({todos,setTodos}) {
+
+  function handleDelete (event, todosID) {
     console.log (event)
     console.log (todosID)
     const result = todos.filter(todos=>todos.id!==todosID)
     setTodos (result)
   }
-}
 
   function handleUpdate (event, TodosID) {
-    const updatedTodos = todos.map(todos => {                        
-      console.log ("The id of the Todos: ", todos.id)
+    const updatedTodos = todos.map(todo => {                        
+      console.log ("The id of the Todos: ", todo.id)
       console.log ("The ID of row that need to be changed", TodosID)
-      if (todos.id === TodosID) {                                     
+      if (todo.id === TodosID) {                                     
         return {                                                   
-          ...todos,
-          status: 'read'
+          ...todo,
+          completed: 'Completed'
         };
       }
-      return todos;                                                 
+      return todo;                                                 
     });
     setTodos(updatedTodos);
   }
+
+
+  return (
+    <>
+    <Table responsive>
+      <thead>
+        <tr>
+          <th>No.</th>
+          <th>To Do List</th>
+          <th>Status</th>
+          <th>Delete</th>
+          <th>Update</th>
+        </tr>
+      </thead>
+      <tbody>
+         {todos.map((todo,index)=>(
+            <tr key={index}>
+              <td>{index+1}</td>
+              <td>{todo.text}</td>
+              <td>{todo.completed}</td>
+              <td><Button variant="danger" name="btnDelete" onClick={(event)=>handleDelete(event,todo.id)}>Delete</Button></td>
+              <td><Button variant="success" name="btnUpdate" onClick={(event)=>handleUpdate(event,todo.id)}>Update</Button></td>             
+            </tr>
+         ))}
+      </tbody>
+    </Table>
+    </>
+  )
+  
+}
+
+  
--- src/Pages/ToDoPage.jsx
+++ src/Pages/ToDoPage.jsx
@@ -4,9 +4,9 @@ import ToDoList from '../Components/ToDoList';
 
 export default function ToDoPage() {
     const [todos, setTodos] = useState([
-    { id: 1, text: 'Learn React', completed: 'Completed' },
-    { id: 2, text: 'Build a Todo App', completed: 'Completed' },
-    { id: 3, text: 'Submit Assignment', completed: 'Completed' }
+    { id: 1, text: 'Learn React', completed: 'Not Completed' },
+    { id: 2, text: 'Build a Todo App', completed: 'Not Completed' },
+    { id: 3, text: 'Submit Assignment', completed: 'Not Completed' }
   ]);
   return (
     <ToDoList todos={todos} setTodos={setTodos}/>
